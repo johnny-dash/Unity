6f8116e666c81731d16ab077146b36213ecccdba
johnny-dash <mzd@mzddeMacBook-Pro.local>
2016-03-08 20:50:32 +1100

    Camera controller and wall

--- Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float speed;
+
 	private Rigidbody rb;
 
 	void Start()
@@ -17,6 +19,6 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertial);
 
-		rb.AddForce (movement);
+		rb.AddForce (movement * speed);
 	}
 }
