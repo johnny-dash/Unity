1c6a146a35227efc3e362bb614eb510f84a0998a
johnny-dash <mzd@mzddeMacBook-Pro.local>
2016-03-10 00:39:54 +1100

    Final version

--- Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countTxT;
+	public Text winTxT;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountValue();
+		winTxT.text = "";
 	}
 
 	void FixedUpdate ()
@@ -22,8 +29,20 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement * speed);
 	}
 
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
 	{
-         Destroy(other.gameObject);
-    }
-}
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1; 
+			setCountValue();
+		}
+	}
+
+	void setCountValue()
+	{
+		countTxT.text = "Count: " + count.ToString();
+		if (count >= 9) {
+			winTxT.text = "You Win!!";
+		}
+	}
+}	
